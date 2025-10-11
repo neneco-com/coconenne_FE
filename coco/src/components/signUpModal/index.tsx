e93655a91b91135494eced2bf1ed8ba7db308cf4
dpqlsn <a01037111209@gmail.com>
2025-10-11 11:13:23 +0900

    feat(#3) :: 회원가입 완료 모달 페이지 퍼블리싱

--- coco/src/components/signUpModal/index.tsx
+++ coco/src/components/signUpModal/index.tsx
@@ -1,3 +1,38 @@
+import * as _ from './style';
+import Image from "next/image";
+
 export default function SignUpModal() {
-  return <></>
+    const lettersTop = [
+        { text: "회", rotate: -8, y: 4 },
+        { text: "원", rotate: 5, y: 0 },
+        { text: "가", rotate: 4, y: 2 },
+        { text: "입", rotate: -3, y: -2 },
+    ];
+
+    const lettersBottom = [
+        { text: "완", rotate: -2, y: 4 },
+        { text: "료", rotate: 5, y: 6 },
+    ];
+
+    return (
+        <_.Container>
+            <Image src="/assets/apply.svg" alt="check" width={80} height={80} />
+            <_.TextGroup>
+                <_.Wrapper>
+                    {lettersTop.map((l, i) => (
+                        <_.Letter key={i} rotate={l.rotate} y={l.y}>
+                            {l.text}
+                        </_.Letter>
+                    ))}
+                </_.Wrapper>
+                <_.Wrapper>
+                    {lettersBottom.map((l, i) => (
+                        <_.Letter key={i} rotate={l.rotate} y={l.y}>
+                            {l.text}
+                        </_.Letter>
+                    ))}
+                </_.Wrapper>
+            </_.TextGroup>
+        </_.Container>
+    );
 }
