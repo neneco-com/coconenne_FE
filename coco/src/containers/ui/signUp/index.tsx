17b665c70ea16a3040886e8366b293cdabfc2e9e
dpqlsn <a01037111209@gmail.com>
2025-10-11 11:00:08 +0900

    feat(#3) :: 회원가입 페이지 퍼블리싱

--- coco/src/containers/ui/signUp/index.tsx
+++ coco/src/containers/ui/signUp/index.tsx
@@ -1,6 +1,15 @@
+'use client';
+
 import * as _ from './style';
+import TextBox from '@/components/textBox';
+import { useState } from 'react';
+import Btn from '@/components/button';
 
 export default function SignUp() {
+  const [miniCode, setMiniCode] = useState('');
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+
   const letters = [
     { text: '회', rotate: -8, y: 4, x: 0 },
     { text: '원', rotate: 5, y: 0, x: 2 },
@@ -17,6 +26,26 @@ export default function SignUp() {
           </_.Letter>
         ))}
       </_.Wrapper>
+      <_.Group>
+        <_.TextGroup>
+          <TextBox
+            value={miniCode}
+            onChange={(e) => setMiniCode(e.target.value)}
+            placeholder="미니코드"
+          />
+          <TextBox
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+            placeholder="아이디"
+          />
+          <TextBox
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="비밀번호"
+          />
+        </_.TextGroup>
+        <Btn>회원가입</Btn>
+      </_.Group>
     </_.Container>
   );
 }
