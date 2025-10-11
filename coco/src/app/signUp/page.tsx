2eabd4cfe922108ef3ed335c6635c16a5a55a2ae
dpqlsn <a01037111209@gmail.com>
2025-10-11 10:35:08 +0900

    feat(#3) :: 회원가입 로직 설계

---
+++ coco/src/app/signUp/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import SignUp from "@/containers/ui/signUp";
+
+export default function SignUpPage() {
+  return <SignUp />;
+}
---
+++ coco/src/containers/ui/signUp/index.tsx
@@ -0,0 +1,9 @@
+import * as _ from './style';
+
+export default function SignUp() {
+  return (
+    <_.Container>
+      <div>SignUp</div>
+    </_.Container>
+  );
+}
