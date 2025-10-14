9663b9b8db5c764f86d167eed3746c0558325bfc
dpqlsn <a01037111209@gmail.com>
2025-10-14 20:03:58 +0900

    feat :: 검색바

--- src/ui/search/index.tsx
+++ src/ui/search/index.tsx
@@ -1,9 +1,17 @@
 import * as _ from './style';
+import Image from 'next/image';
 
-export default function Search(){
+export default function Search() {
     return (
         <_.Container>
-            
+            <_.Wrapper>
+                <Image src='assets/arrow.svg' alt='arrow' width={24} height={24} />
+                <_.Title>검색하기</_.Title>
+            </_.Wrapper>
+            <_.SearchWrapper>
+                <Image src='assets/itemSearch.svg' alt='itemSearch' width={24} height={24} />
+                <_.SearchInput placeholder="원하는 상품을 검색하세요" />
+            </_.SearchWrapper>
         </_.Container>
     );
 }
--- src/ui/search/style.ts
+++ src/ui/search/style.ts
@@ -1,7 +1,47 @@
 import styled from "@emotion/styled";
+import { Gangwon } from "@/styles/Font";
 
 export const Container = styled.div`
   display: flex;
-  flex-direction: column;
   align-items: center;
+  flex-direction: column;
+  width: 400px;
+  height: 75vh;
+  padding: 0.8rem 1rem;
+  gap: 1.5rem;
+`;
+
+export const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 9rem;
+    width: 400px;
+`;
+
+export const Title = styled.div`
+    font-size: 1.25rem;
+`;
+
+export const SearchWrapper = styled.div`
+    ${Gangwon}
+    display: flex;
+    align-items: center;
+    width: 100%;
+    background-color: #F5F7F8;
+    padding: 0.8rem 1.2rem;
+    border-radius: 999px;
+    gap: 0.5rem;
+`;
+
+export const SearchInput = styled.input`
+    ${Gangwon}
+    border: none;
+    outline: none;
+    background: transparent;
+    flex: 1;
+    font-size: 1.2rem;
+
+    &::placeholder {
+        color: #B5BCBF;
+    }
 `;
