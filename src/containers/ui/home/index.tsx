ae606ccc133f2ace098e4b4b345f05907587b849
dpqlsn <a01037111209@gmail.com>
2025-10-14 19:28:58 +0900

    feat :: search 페이지로 이동

--- src/containers/ui/home/index.tsx
+++ src/containers/ui/home/index.tsx
@@ -1,7 +1,10 @@
 import * as _ from './style';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+    const router = useRouter();
+
     return (
         <_.Container>
             <_.Wrapper>
@@ -9,11 +12,11 @@ export default function Home() {
                 <_.SubTitle>항목 선택 시 다음 화면으로 즉시 이동합니다</_.SubTitle>
             </_.Wrapper>
             <_.Group>
-                <_.Box>
-                    <Image src="/assets/serach.svg" alt="serach" width={50} height={50} />
+                <_.Box onClick={() => router.push('/search')}>
+                    <Image src="/assets/search.svg" alt="search" width={50} height={50} />
                     검색하기
                 </_.Box>
-                <_.Box>
+                <_.Box onClick={() => router.push('/exchange')}>
                     <Image src="/assets/exchange.svg" alt="exchange" width={50} height={50} />
                     교환하기
                 </_.Box>
